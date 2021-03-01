609ed9555e86cd3da0de8d135a1b531e9a45890d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-28 21:40:29 -0500

    Created cnn.py and main.py

---
+++ cnn/main.py
@@ -0,0 +1,29 @@
+import torch 
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+# Hyper parameters
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+learning_rate = 0.001
+
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True, 
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                          train=False, 
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size, 
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size, 
+                                          shuffle=False)
