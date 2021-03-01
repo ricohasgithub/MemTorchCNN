d39796179f41d4a3145003eb19cc1b26ce993c2d
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2021-02-28 22:12:45 -0500

    Created Memristor Analogs

---
+++ memristor_cnn/main.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
+
+import memtorch
+from memtorch.utils import LoadCIFAR10
+
+# Create new reference memristor
+reference_memristor = memtorch.bh.memristor.VTEAM
+reference_memristor_params = {'time_series_resolution': 1e-10}
+memristor = reference_memristor(**reference_memristor_params)
+memristor.plot_hysteresis_loop()
+
+print("Hello world")
